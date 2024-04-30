7cea7d171edf030fec15380ad0ca70c981f762d6
camilot26 <camilotejada1@gmail.com>
2024-04-30 11:28:29 -0500

    crear carpetas

---
+++ salonBelleza/src/main/java/com/riwi/salonBelleza/SalonBellezaApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.salonBelleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonBellezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonBellezaApplication.class, args);
+	}
+
+}
